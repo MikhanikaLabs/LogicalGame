32ec0eba83216e94fc8947e5fe352f8a1d315f4c
MikhanikaLabs <MikhanikaLabs@gmail.com>
2020-01-05 16:15:47 +0300

    Updates

---
+++ precache-manifest.b559fefda8999f7f37d285d0f5070881.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9863e3707822e0d3711",
+    "url": "/logicalgame/static/js/runtime~main.056708f2.js"
+  },
+  {
+    "revision": "7056676514f55777d99e",
+    "url": "/logicalgame/static/js/main.930b774e.chunk.js"
+  },
+  {
+    "revision": "2ef0533e574cf7e3c5b8",
+    "url": "/logicalgame/static/js/2.98495afe.chunk.js"
+  },
+  {
+    "revision": "7056676514f55777d99e",
+    "url": "/logicalgame/static/css/main.adce0620.chunk.css"
+  },
+  {
+    "revision": "620aea8024e591caa5908b39ba0969d1",
+    "url": "/logicalgame/index.html"
+  }
+];
